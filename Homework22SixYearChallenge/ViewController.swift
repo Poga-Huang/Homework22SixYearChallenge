547e76bd637af04dfb2b1168df5c98d006f571a8
Poga Huang <pgh86812@gmail.com>
2021-11-25 15:16:39 +0800

    Initial Commit

---
+++ Homework22SixYearChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework22SixYearChallenge
+//
+//  Created by 黃柏嘉 on 2021/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
